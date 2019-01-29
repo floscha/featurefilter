805d4c71169b53c6eb3f89bdb7a31b83e57b0147
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-01-29 20:57:45 +0100

    Make fit() return None and add default implementation of fit_transform()

--- featurefilter/abstract_transformer.py
+++ featurefilter/abstract_transformer.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 
-class AbstractTransformer():
+class AbstractTransformer:
     """An abstract class that provides an sklearn-like API."""
-    def fit(self, df: pd.DataFrame) -> pd.DataFrame:
+    def fit(self, df: pd.DataFrame) -> None:
         raise NotImplementedError()
 
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError()
 
     def fit_transform(self, df: pd.DataFrame) -> pd.DataFrame:
-        raise NotImplementedError()
+        self.fit(df)
+        return self.transform(df)
