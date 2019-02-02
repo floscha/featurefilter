d9e9e45b1e140089bc8b13c884f5493ec53e4477
Florian Schäfer <33159547+floscha@users.noreply.github.com>
2019-02-02 22:47:37 +0100

    Move tests out of useless TestCase

--- tests/test_abstract_transformer.py
+++ tests/test_abstract_transformer.py
@@ -5,24 +5,23 @@ import pytest
 from featurefilter.abstract_transformer import AbstractTransformer
 
 
-class AbstractTransformerTest(unittest.TestCase):
-    def test_fit_raises_not_implemented_error(self):
-        test_transformer = AbstractTransformer()
+def test_fit_raises_not_implemented_error():
+    test_transformer = AbstractTransformer()
 
-        with pytest.raises(NotImplementedError):
-            test_transformer.fit(None)
+    with pytest.raises(NotImplementedError):
+        test_transformer.fit(None)
 
-    def test_transform_raises_not_implemented_error(self):
-        test_transformer = AbstractTransformer()
+def test_transform_raises_not_implemented_error():
+    test_transformer = AbstractTransformer()
 
-        with pytest.raises(NotImplementedError):
-            test_transformer.transform(None)
+    with pytest.raises(NotImplementedError):
+        test_transformer.transform(None)
 
-    def test_fit_transform_raises_not_implemented_error(self):
-        test_transformer = AbstractTransformer()
+def test_fit_transform_raises_not_implemented_error():
+    test_transformer = AbstractTransformer()
 
-        with pytest.raises(NotImplementedError):
-            test_transformer.fit_transform(None)
+    with pytest.raises(NotImplementedError):
+        test_transformer.fit_transform(None)
 
 
 if __name__ == '__main__':
