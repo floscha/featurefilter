936d3c1264edc17bfcd45a840c71cb434cbd4481
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-02-12 22:10:46 +0100

    Fix linter issues for unit tests

--- tests/test_abstract_transformer.py
+++ tests/test_abstract_transformer.py
@@ -11,11 +11,13 @@ def test_fit_raises_not_implemented_error():
     with pytest.raises(NotImplementedError):
         test_transformer.fit(None)
 
+
 def test_transform_raises_not_implemented_error():
     test_transformer = AbstractTransformer()
 
     with pytest.raises(NotImplementedError):
         test_transformer.transform(None)
+
 
 def test_fit_transform_raises_not_implemented_error():
     test_transformer = AbstractTransformer()
