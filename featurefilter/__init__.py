0e4a51a4d2925c0d52fcfb7bb19e70e1ca26a30c
Florian Schäfer <33159547+floscha@users.noreply.github.com>
2019-02-13 18:12:46 +0100

    Remove uninformative variables using tree-based models (resolves #16)

--- featurefilter/__init__.py
+++ featurefilter/__init__.py
@@ -1,5 +1,6 @@
 from .na_filter import NaFilter
+from .tree_based_filter import TreeBasedFilter
 from .variance_filter import VarianceFilter
 
 
-__all__ = ['NaFilter', 'VarianceFilter']
+__all__ = ['NaFilter', 'TreeBasedFilter', 'VarianceFilter']
