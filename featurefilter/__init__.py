188def6e35dab499decf9c55ee3df513c4a80c6e
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-02-13 23:29:50 +0100

    Add FeatureCorrelationFilter to module init

--- featurefilter/__init__.py
+++ featurefilter/__init__.py
@@ -1,6 +1,8 @@
+from .feature_correlation_filter import FeatureCorrelationFilter
 from .na_filter import NaFilter
 from .tree_based_filter import TreeBasedFilter
 from .variance_filter import VarianceFilter
 
 
-__all__ = ['NaFilter', 'TreeBasedFilter', 'VarianceFilter']
+__all__ = ['FeatureCorrelationFilter', 'NaFilter', 'TreeBasedFilter',
+           'VarianceFilter']
