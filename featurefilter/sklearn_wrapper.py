02273d84f1bb74df7dbb17b8457a280e87dc02ef
Florian Schäfer <33159547+floscha@users.noreply.github.com>
2019-03-10 17:27:37 +0100

    Add verbosity to SklearnWrapper + respective unit tests (resolves #26)

--- featurefilter/sklearn_wrapper.py
+++ featurefilter/sklearn_wrapper.py
@@ -1,7 +1,10 @@
 from typing import List  # NOQA
 
+import numpy as np
 import pandas as pd
-from sklearn import feature_selection as sklearn_feature_selection
+from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection.base import SelectorMixin
+from sklearn.feature_selection.from_model import _get_feature_importances
 from sklearn.feature_selection.univariate_selection import _BaseFilter
 
 from .abstract_transformer import AbstractTransformer
@@ -10,7 +13,7 @@ from .abstract_transformer import AbstractTransformer
 class SklearnWrapper(AbstractTransformer):
     """Remove variables using sklearn's feature_selection module."""
     def __init__(self,
-                 selector: sklearn_feature_selection.base.SelectorMixin,
+                 selector: SelectorMixin,
                  target_column: str = None,
                  sample_ratio: float = 1.0,
                  seed: int = None,
@@ -27,6 +30,55 @@ class SklearnWrapper(AbstractTransformer):
         self.verbose = verbose
 
         self.columns_to_drop = []  # type: List[str]
+        self._indices_to_drop = []  # type: List[str]
+
+    def print_columns_to_drop(self):
+        """Print information if and why a column was dropped."""
+        for ci, cn in zip(self._indices_to_drop, self.columns_to_drop):
+            selector_attributes = self.selector.__dict__
+            if 'threshold' in selector_attributes:
+                # VarianceThreshold
+                if self.selector.__class__.__name__ == 'VarianceThreshold':
+                    print(("The variance of column '%s' (%0.4f) is " +
+                           "below the threshold of %0.4f")
+                          % (cn, self.selector.variances_[ci],
+                             self.selector.threshold))
+                # SelectFromModel
+                if self.selector.__class__.__name__ == 'SelectFromModel':
+                    # The fitted estimator ends with an underscore
+                    estimator = self.selector.estimator_
+                    print(("The feature importance of column '%s' " +
+                           "(%0.4f) is below the threshold of %0.4f")
+                          % (cn, _get_feature_importances(estimator)[ci],
+                             self.selector.threshold))
+            elif 'percentile' in selector_attributes:
+                # SelectPercentile
+                print(("The feature importance of column '%s' (%0.4f) is " +
+                       "out of the %d%% of features to keep")
+                      % (cn, self.selector.scores_[ci],
+                         self.selector.percentile))
+            elif 'alpha' in selector_attributes:
+                # SelectFpr, SelectFdr, SelectFwe
+                print(("The p-value of column '%s' (%0.4f) is above the " +
+                       "specified alpha of %0.4f")
+                      % (cn, self.selector.pvalues_[ci], self.selector.alpha))
+            elif 'k' in selector_attributes:
+                # SelectKBest
+                print(("The feature importance of column '%s' (%0.4f) is " +
+                       "too low to end up in the %d best features")
+                      % (cn, self.selector.scores_[ci], self.selector.k))
+            elif 'n_features_to_select' in selector_attributes:
+                # RFE
+                estimator = self.selector.estimator_
+                print(("The feature importance of column '%s' is " +
+                       "too low to end up in the %d best features")
+                      % (cn, self.selector.n_features_to_select))
+            elif 'min_features_to_select' in selector_attributes:
+                # RFECV
+                estimator = self.selector.estimator_
+                print(("The feature importance of column '%s' is " +
+                       "too low to end up in the %d best features")
+                      % (cn, self.selector.min_features_to_select))
 
     def fit(self, df: pd.DataFrame, *args, **kwargs) -> None:
         column_names = df.columns
@@ -35,13 +87,15 @@ class SklearnWrapper(AbstractTransformer):
             self.selector.fit(df[column_names], df[self.target_column])
         else:
             self.selector.fit(df)
-        support_mask = self.selector._get_support_mask()
+        support_mask = self.selector.get_support()
         # Inverse support_mask since it contains the columns to keep
         self.columns_to_drop = list(column_names[~support_mask])
+        self._indices_to_drop = np.arange(len(column_names))[~support_mask]
+        if self.verbose:
+            self.print_columns_to_drop()
 
     def transform(self, df: pd.DataFrame, *args, **kwargs) -> pd.DataFrame:
-        if (isinstance(self.selector,
-                       sklearn_feature_selection.SelectFromModel)
+        if (isinstance(self.selector, SelectFromModel)
                 and self.selector.prefit):
             column_names = df.columns.drop(self.target_column)
             support_mask = self.selector._get_support_mask()
--- tests/test_sklearn_wrapper.py
+++ tests/test_sklearn_wrapper.py
@@ -1,8 +1,12 @@
+import contextlib
+from io import StringIO
 import unittest
 
 import pandas as pd
 import pytest
-from sklearn import feature_selection as sklearn_feature_selection
+from sklearn.feature_selection import (chi2, RFE, RFECV, SelectFdr, SelectFpr,
+                                       SelectFromModel, SelectFwe, SelectKBest,
+                                       SelectPercentile, VarianceThreshold)
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 
@@ -13,9 +17,7 @@ def test_removing_features_with_low_variance():
     train_df = pd.DataFrame({'A': [0, 1, 2], 'B': [0, 0, 0]})
     test_df = pd.DataFrame({'A': [0, 0, 0], 'B': [0, 1, 2]})
 
-    sklearn_selector = sklearn_feature_selection.VarianceThreshold(
-        threshold=0.5
-    )
+    sklearn_selector = VarianceThreshold(threshold=0.5)
     variance_threshold = SklearnWrapper(sklearn_selector)
     train_df = variance_threshold.fit_transform(train_df)
     test_df = variance_threshold.transform(test_df)
@@ -34,8 +36,7 @@ def test_rfe():
     expected_output = pd.DataFrame({'A': [0, 0, 1, 1],
                                     'Y': [0, 0, 1, 1]})
 
-    model = sklearn_feature_selection.RFE(LinearRegression(),
-                                          n_features_to_select=1)
+    model = RFE(LinearRegression(), n_features_to_select=1)
     selector = SklearnWrapper(model, target_column='Y')
     train_df = selector.fit_transform(train_df)
 
@@ -49,9 +50,7 @@ def test_rfecv():
     expected_output = pd.DataFrame({'A': [0, 0, 1, 1],
                                     'Y': [0, 0, 1, 1]})
 
-    model = sklearn_feature_selection.RFECV(LinearRegression(),
-                                            min_features_to_select=1,
-                                            cv=3)
+    model = RFECV(LinearRegression(), min_features_to_select=1, cv=3)
     selector = SklearnWrapper(model, target_column='Y')
     train_df = selector.fit_transform(train_df)
 
@@ -65,10 +64,7 @@ def test_univariate_feature_selection():
     expected_output = pd.DataFrame({'A': [0, 0, 1, 1],
                                     'Y': [0, 0, 1, 1]})
 
-    k_best_selector = sklearn_feature_selection.SelectKBest(
-        sklearn_feature_selection.chi2,
-        k=1
-    )
+    k_best_selector = SelectKBest(chi2, k=1)
     selector = SklearnWrapper(k_best_selector, target_column='Y')
     train_df = selector.fit_transform(train_df)
 
@@ -76,10 +72,7 @@ def test_univariate_feature_selection():
 
 
 def test_missing_target_column():
-    k_best_selector = sklearn_feature_selection.SelectKBest(
-        sklearn_feature_selection.chi2,
-        k=1
-    )
+    k_best_selector = SelectKBest(chi2, k=1)
 
     with pytest.raises(ValueError) as excinfo:
         SklearnWrapper(k_best_selector)
@@ -94,8 +87,7 @@ def test_glm_based_feature_selection():
     expected_output = pd.DataFrame({'A': [0, 0, 1, 1],
                                     'Y': [0, 0, 1, 1]})
 
-    model = sklearn_feature_selection.SelectFromModel(LinearRegression(),
-                                                      threshold=0.5)
+    model = SelectFromModel(LinearRegression(), threshold=0.5)
     selector = SklearnWrapper(model, target_column='Y')
     train_df = selector.fit_transform(train_df)
 
@@ -111,9 +103,7 @@ def test_prefit_model():
 
     linear_regression = LinearRegression()
     linear_regression.fit(train_df[['A', 'B']], train_df['Y'])
-    model = sklearn_feature_selection.SelectFromModel(linear_regression,
-                                                      prefit=True,
-                                                      threshold=0.5)
+    model = SelectFromModel(linear_regression, prefit=True, threshold=0.5)
     selector = SklearnWrapper(model, target_column='Y')
     train_df = selector.transform(train_df)
 
@@ -127,12 +117,130 @@ def test_tree_based_feature_selection():
     expected_output = pd.DataFrame({'A': [0, 0, 1, 1],
                                     'Y': [0, 0, 1, 1]})
 
-    model = sklearn_feature_selection.SelectFromModel(DecisionTreeRegressor(),
-                                                      threshold=0.5)
+    model = SelectFromModel(DecisionTreeRegressor(), threshold=0.5)
     selector = SklearnWrapper(model, target_column='Y')
     train_df = selector.fit_transform(train_df)
 
     assert train_df.equals(expected_output)
+
+
+def _capture_verbose_output_for_model(model, use_supervised_df):
+    if use_supervised_df:
+        df = pd.DataFrame({'A': [0, 0, 1, 1],
+                           'B': [0, 1, 0, 1],
+                           'Y': [0, 0, 1, 1]})
+        target_column = 'Y'
+    else:
+        df = pd.DataFrame({'A': [0, 1, 2], 'B': [0, 0, 0]})
+        target_column = None
+
+    filter = SklearnWrapper(model, target_column)
+    filter.fit(df)
+
+    temp_stdout = StringIO()
+    with contextlib.redirect_stdout(temp_stdout):
+        filter.print_columns_to_drop()
+    output = temp_stdout.getvalue().strip()
+
+    return output
+
+
+def test_verbose_output_for_variance_threshold():
+    expected_output = ("The variance of column 'B' (0.0000) is below the " +
+                       "threshold of 0.5000")
+    model = VarianceThreshold(threshold=0.5)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=False)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_from_model():
+    expected_output = ("The feature importance of column 'B' (0.0000) is " +
+                       "below the threshold of 0.5000")
+
+    model = SelectFromModel(LinearRegression(), threshold=0.5)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_percentile():
+    expected_output = ("The feature importance of column 'B' (0.0000) is " +
+                       "out of the 10% of features to keep")
+
+    model = SelectPercentile(chi2, percentile=10)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_select_fpr():
+    expected_output = ("The p-value of column 'B' (1.0000) is above the " +
+                       "specified alpha of 0.5000")
+
+    model = SelectFpr(chi2, alpha=0.5)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_select_fdr():
+    expected_output = ("The p-value of column 'B' (1.0000) is above the " +
+                       "specified alpha of 0.5000")
+
+    model = SelectFdr(chi2, alpha=0.5)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_select_fwe():
+    expected_output = ("The p-value of column 'B' (1.0000) is above the " +
+                       "specified alpha of 0.5000")
+
+    model = SelectFwe(chi2, alpha=0.5)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_select_k_best():
+    expected_output = ("The feature importance of column 'B' (0.0000) is " +
+                       "too low to end up in the 1 best features")
+
+    model = SelectKBest(chi2, k=1)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_rfe():
+    expected_output = ("The feature importance of column 'B' is " +
+                       "too low to end up in the 1 best features")
+
+    model = RFE(LinearRegression(), n_features_to_select=1)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
+
+
+def test_verbose_output_for_select_rfecv():
+    expected_output = ("The feature importance of column 'B' is " +
+                       "too low to end up in the 1 best features")
+
+    model = RFECV(LinearRegression(), min_features_to_select=1, cv=3)
+
+    output = _capture_verbose_output_for_model(model, use_supervised_df=True)
+
+    assert output == expected_output
 
 
 if __name__ == '__main__':
