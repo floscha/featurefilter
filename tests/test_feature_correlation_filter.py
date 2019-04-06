3c1f4aad941c6e0c14ee80e7d7662bf00b88b846
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-04-06 16:47:12 +0200

    Avoid redefining built-in 'filter'

--- tests/test_feature_correlation_filter.py
+++ tests/test_feature_correlation_filter.py
@@ -9,10 +9,10 @@ def test_fit_high_continuous_correlation():
     train_df = pd.DataFrame({'A': [0, 1],
                              'B': [0, 1]})
 
-    filter = FeatureCorrelationFilter()
-    train_df = filter.fit(train_df)
+    filter_ = FeatureCorrelationFilter()
+    train_df = filter_.fit(train_df)
 
-    assert filter.columns_to_drop == ['B']
+    assert filter_.columns_to_drop == ['B']
 
 
 def test_excluding_target_column():
@@ -20,19 +20,19 @@ def test_excluding_target_column():
                              'B': [0, 1],
                              'Y': [0, 1]})
 
-    filter = FeatureCorrelationFilter(target_column='Y')
-    train_df = filter.fit(train_df)
+    filter_ = FeatureCorrelationFilter(target_column='Y')
+    train_df = filter_.fit(train_df)
 
-    assert filter.columns_to_drop == ['B']
+    assert filter_.columns_to_drop == ['B']
 
 
 def test_high_negative_continuous_correlation():
     train_df = pd.DataFrame({'A': [0, 1], 'B': [0, -1], 'Y': [0, 1]})
     test_df = pd.DataFrame({'A': [0, 0], 'B': [0, 0], 'Y': [0, 1]})
 
-    filter = FeatureCorrelationFilter(target_column='Y')
-    train_df = filter.fit_transform(train_df)
-    test_df = filter.transform(test_df)
+    filter_ = FeatureCorrelationFilter(target_column='Y')
+    train_df = filter_.fit_transform(train_df)
+    test_df = filter_.transform(test_df)
 
     assert train_df.equals(pd.DataFrame({'A': [0, 1], 'Y': [0, 1]}))
     assert test_df.equals(pd.DataFrame({'A': [0, 0], 'Y': [0, 1]}))
@@ -42,9 +42,9 @@ def test_categorical_correlation():
     train_df = pd.DataFrame({'A': ['a', 'b'], 'B': ['a', 'b'], 'Y': [0, 1]})
     test_df = pd.DataFrame({'A': ['a', 'b'], 'B': ['b', 'b'], 'Y': [0, 1]})
 
-    filter = FeatureCorrelationFilter(target_column='Y')
-    train_df = filter.fit_transform(train_df)
-    test_df = filter.transform(test_df)
+    filter_ = FeatureCorrelationFilter(target_column='Y')
+    train_df = filter_.fit_transform(train_df)
+    test_df = filter_.transform(test_df)
 
     assert train_df.equals(pd.DataFrame({'A': ['a', 'b'], 'Y': [0, 1]}))
     assert test_df.equals(pd.DataFrame({'A': ['a', 'b'], 'Y': [0, 1]}))
