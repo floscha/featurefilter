ae7101b72c776e7b2851a9554b70b3156ff37b38
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-04-24 21:21:14 +0200

    Bump version to 0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='featurefilter',
-    version='0.1',
+    version='0.11',
     description='A Python library for removing uninformative variables from datasets',
     url='https://github.com/floscha/featurefilter/',
     author='Florian Schäfer',
